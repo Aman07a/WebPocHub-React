947b715c46ed2f6c6ec556bba4a52a429534621b
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-13 15:26:01 +0100

    Testing ASP.NET Core Web API endpoints

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to WebPoCHub Web API Training!</h1>
+      <hr />
+      <h6>Designed by Aman Ahmed!</h6>
+    </div>
+  );
+}
+
+export default App;
