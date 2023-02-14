e7aa2debd03e75e5282a3a77acb7ad2d2ceb723a
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-14 07:57:56 +0100

    Added bearerToken

--- src/Services/employees-service.js
+++ src/Services/employees-service.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 class EmployeesService {
     #_baseUrl = "https://localhost:7274/api";
+    bearerToken = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGFiYWRlLnByYXZpbmt1bWFyQHdlYnBvY2h1Yi5jb20iLCJyb2xlIjoiRW1wbG95ZWUiLCJuYmYiOjE2NzYzNTcxNDcsImV4cCI6MTY3NjM2MDc0NywiaWF0IjoxNjc2MzU3MTQ3fQ.5GkpAm7uuhJ61ypwVkumuObP7ZsOnTFardaomBLGL6hYuEnrPhm_wDhi1HIGiwEZmt2SHGzfxFJ0-GFFt3cmog";
 
     async getAllEmployees() {
         return await (
             await axios.get(`${this.#_baseUrl}/employees`, {
                 headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGFiYWRlLnByYXZpbmt1bWFyQHdlYnBvY2h1Yi5jb20iLCJyb2xlIjoiRW1wbG95ZWUiLCJuYmYiOjE2NzYzMDA2MzQsImV4cCI6MTY3NjMwNDIzNCwiaWF0IjoxNjc2MzAwNjM0fQ.4zQxpD7-OXNunQLF0MZBcNo0u9vQzZNisoLfxnOJdj17j34COUzewEmNFPWHxAoF4hu09yKdGh3diFaqZXHFAA"
+                    "Authorization": `Bearer ${this.bearerToken}` 
                 }
             })
         ).data;
